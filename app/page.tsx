5b659ed7783ae759b7cb731582108c0df61a8da5
waltodd <waltodd@hotmail.com>
2024-01-22 00:19:30 -0500

    Media Query added

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,123 @@
+"use client"
+import { useState } from "react";
 import Image from "next/image";
 
+interface Props {
+  value: string;
+  label: string;
+}
+const languageOptions = [
+  { value: 'auto', label: 'Detect Language' },
+  { value: 'en', label: 'English' },
+  { value: 'fr', label: 'French' },
+  { value: 'es', label: 'Spanish' },
+];
+
+const languageOptionsResult = [
+  { value: 'en', label: 'English' },
+  { value: 'fr', label: 'French' },
+  { value: 'es', label: 'Spanish' },
+];
 export default function Home() {
+  const [inputText, setInputText] = useState('');
+  const [selectedLanguage, setSelectedLanguage] = useState(languageOptions[0]);
+  const [translatedText, setTranslatedText] = useState('');
+  const [active, setActive] = useState(0);
+  const [activeResult, setActiveResult] = useState(0);
+
+  const apiKey = 'YOUR_GOOGLE_API_KEY'; // Replace with your API key
+
+  // const handleTranslate = async () => {
+  //   try {
+  //     let targetLanguage = selectedLanguage.value;
+  //     if (targetLanguage === 'auto') {
+  //       targetLanguage = undefined; // Detect language automatically
+  //     }
+
+  //     const result = await translate(inputText, { to: targetLanguage, key: apiKey });
+  //     setTranslatedText(result.text);
+  //   } catch (error) {
+  //     console.error('Translation error:', error);
+  //   }
+  const handleSelectLanguage = (index: number) => {
+    console.log(index)
+    setActive(index)
+  }
+
+  const handleSelectLanguageResult = (index: number) => {
+    console.log(index)
+    setActiveResult(index)
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="hero-section">
+      <div className="main-container">
+        <div className="logo">
+          <Image src="/assets/logo.svg" alt="logo" width={200} height={200} />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="card-container">
+          <div className="card">
+            <div className="card-header">
+              {languageOptions.map((language, index) => (
+                <div onClick={() => handleSelectLanguage(index)}
+                  className={`btn-language ${active === index ? 'active' : ''}`} key={index}>
+                  {language.label}
+                </div>
+              ))}
+            </div>
+            <div className="card-content">
+              <textarea className="textare-input" />
+              <p>19/500</p>
+            </div>
+            <div className="card-footer">
+              <div className="btn-icon-container">
+                <div className="btn-icon">
+                  <Image src="/assets/sound_max_fill.svg" width={25} height={25} alt="sound" />
+                </div>
+                <div className="btn-icon">
+                  <Image src="/assets/Copy.svg" width={25} height={25} alt="copy" />
+                </div>
+              </div>
+              <div>
+                <div className="btn-translate">
+                  <Image className="img" src="/assets/Sort_alfa.svg" width={25} height={25} alt="copy" />
+                  Translate
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-header-result">
+              <div className="result">
+              {languageOptionsResult.map((language, index) => (
+                <div onClick={() => handleSelectLanguageResult(index)}
+                  className={`btn-language ${activeResult === index ? 'active' : ''}`} key={index}>
+                  {language.label}
+                </div>
+              ))}
+              </div>
+              <div>
+              <div className="btn-icon">
+                <Image src="/assets/Horizontal_top_left_main.svg" width={25} height={25} alt="sound" />
+              </div>
+              </div>
+            </div>
+            <div className="card-content">
+              <textarea className="textare-input" />
+            </div>
+            <div className="card-footer">
+              <div className="btn-icon-container">
+                <div className="btn-icon">
+                  <Image src="/assets/sound_max_fill.svg" width={25} height={25} alt="sound" />
+                </div>
+                <div className="btn-icon">
+                  <Image src="/assets/Copy.svg" width={25} height={25} alt="copy" />
+                </div>
+              </div>
+              <div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
