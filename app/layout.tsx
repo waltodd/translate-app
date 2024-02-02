e51a13f33b8067e5df50fa2da80c231a539fb790
waltodd <waltodd@hotmail.com>
2024-02-01 22:41:14 -0500

    add HEAD

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const dm_sans = DM_Sans({ subsets: ["latin"] });
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+      <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
+      </Head>
       <body className={`${dm_sans.className}`}>{children}</body>
     </html>
   );
